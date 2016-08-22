700e46cb69317362e1d203f2822c6ca4a040574d
Sung-Hyun Kang <usrhyu@kang.workpc.tds.net>
2016-08-22 08:14:55 -0500

    second commit

---
+++ main.js
@@ -0,0 +1,21 @@
+document.addEventListener("animationstart", insertDivContent, false);
+
+if (document.readyState ==='complete'){
+}
+
+
+
+function insertDivContent(){
+  alert("HELLO");
+  table = document.querySelectorAll("table.CG_list.searchList")[0];
+  div = table.getElementsByClassName("sectionExpandColumn");
+  alert(div.length);
+  
+  var button = document.createElement("a");
+  button.setAttribute("class", "sectionExpand collapsibleCriteria");
+  button.innerHTML = "Discussion";
+
+  for (var i=0; i<div.length; i++){
+    div[i].appendChild(button);
+  }
+}
