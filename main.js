fb6a1a846c168ca40db6fffd53d6e100b2566c0e
Sung-Hyun Kang <love.power.yeah@gmail.com>
2016-09-13 12:56:04 -0500

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ if (document.readyState ==='complete'){
 
 
 function insertDivContent(){
-  log.info("Insertinf Div");
+  console.log("Inserting Div");
   table = document.querySelectorAll("table.CG_list.searchList")[0];
   div = table.getElementsByClassName("sectionExpandColumn");
   alert(div.length);
